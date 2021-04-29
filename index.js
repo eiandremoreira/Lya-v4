239f02ad8d671671de2d9bdfb5e6d22a3d4d660e
lrd <73443391+L0R4@users.noreply.github.com>
2021-04-29 14:48:48 -0300

    Nova index

--- index.js
+++ index.js
@@ -1,22 +1,4 @@
 // Requires
 require("colors");
-require("eris-reactions");
-require("./src/complementos/mention");
-require("eris-collector");
-require("eris-additions");
+require("./src/complementos/Bot_Chan");
 
-// Handler
-class Bot_Chan extends require("./src/handler_comandos/index").Client {
-   constructor() {
-      super({
-         token: require("./src/jsons/client.config.json").token,
-         eventsPath: "./src/eventos",
-         commandsPath: "./src/comandos"
-      });
-   }
-}
-
-// Conexão com API
-new Bot_Chan(require("./src/jsons/client.config.json").token, {
-   restMode: true
-}).connect(); 
