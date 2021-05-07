a7d55640e315f00190ddbf99288d1372bcc11c74
MrAndre <71835146+eiandremoreira@users.noreply.github.com>
2021-05-07 09:23:19 -0300

    Update Bot_Chan.js

--- src/complementos/Bot_Chan.js
+++ src/complementos/Bot_Chan.js
@@ -14,7 +14,8 @@ class Bot_Chan extends require("../handler_comandos/index").Client {
  // Criação do Client
  const client = new Bot_Chan(`Bot ${require("../jsons/client.config.json").token}`, {
     restMode: true,
-    intents: 1667
+    intents: 1667,
+    defaultImageSize: 2048
  })
 
  // Comandos Slash
